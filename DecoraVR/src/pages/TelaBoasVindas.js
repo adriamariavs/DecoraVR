3adf4b345d11a1eba5f2baa75136edf2def266be
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-06-19 21:58:41 -0300

    commit2

--- DecoraVR/src/pages/TelaBoasVindas.js
+++ DecoraVR/src/pages/TelaBoasVindas.js
@@ -1,15 +1,26 @@
 import { ImageBackground, Text } from "react-native";
 import { styles } from "../Styles/styles";
 import { View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function TelaBoasVindas() {
+  const [fontsLoaded] = useFonts({
+    CrimsonText: require("../assets/Fonts/CrimsonText-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return undefined;
+  }
+
   return (
     <ImageBackground
       source={require("../image/TelaBoas-Vindas.png")}
       style={styles.ImgBoasVindas}
     >
       <View>
-        <Text style={{ fontSize: 30 }}>MAKE YOUR</Text>
+        <Text style={{ fontSize: 30, fontFamily: "CrimsonText" }}>
+          MAKE YOUR
+        </Text>
       </View>
     </ImageBackground>
   );
