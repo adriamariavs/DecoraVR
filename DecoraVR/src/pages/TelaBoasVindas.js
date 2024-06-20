fc65cdbf13a6e080bf9d5539c9b1eefe0bb2694d
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-06-19 21:43:19 -0300

    commit

--- DecoraVR/src/pages/TelaBoasVindas.js
+++ DecoraVR/src/pages/TelaBoasVindas.js
@@ -1,18 +1,16 @@
 import { ImageBackground, Text } from "react-native";
 import { styles } from "../Styles/styles";
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { View } from "react-native";
 
-export default function TelaBoasVindas () {
+export default function TelaBoasVindas() {
   return (
-    <ImageBackground source={require("../image/TelaBoas-Vindas.png")} 
-     style={(styles.ImgBoasVindas)} >
-
-     <View>
-     <Text style={{ fontSize: 30 }}>
-     MAKE YOUR
-     </Text>
-     </View>
-
+    <ImageBackground
+      source={require("../image/TelaBoas-Vindas.png")}
+      style={styles.ImgBoasVindas}
+    >
+      <View>
+        <Text style={{ fontSize: 30 }}>MAKE YOUR</Text>
+      </View>
     </ImageBackground>
-  )
+  );
 }
